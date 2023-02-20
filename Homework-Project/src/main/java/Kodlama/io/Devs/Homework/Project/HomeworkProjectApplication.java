e3623c371b294a57ec9315ec05b07535a50690c8
Şahin Yakıcı <shnykc@hotmail.com>
2023-02-20 10:07:29 +0300

    Initial commit

---
+++ Homework-Project/src/main/java/Kodlama/io/Devs/Homework/Project/HomeworkProjectApplication.java
@@ -0,0 +1,13 @@
+package Kodlama.io.Devs.Homework.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkProjectApplication.class, args);
+	}
+
+}
---
+++ Homework-Project/src/test/java/Kodlama/io/Devs/Homework/Project/HomeworkProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Kodlama.io.Devs.Homework.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
